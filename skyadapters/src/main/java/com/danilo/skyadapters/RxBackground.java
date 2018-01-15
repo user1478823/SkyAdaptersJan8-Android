04a3f3cd07b1efb4424be1ab913a1864640d2a37
Danilo <danilot.89@gmail.com>
2018-01-15 15:28:45 +0100

    commit

--- skyadapters/src/main/java/com/danilo/skyadapters/RxBackground.java
+++ skyadapters/src/main/java/com/danilo/skyadapters/RxBackground.java
@@ -17,7 +17,7 @@ import io.reactivex.schedulers.Schedulers;
 
 public class RxBackground {
 
-    public void executeInBackground(RvBase a, final RxBackgroundInterface rxBackgroundInterface) {
+    public void executeInBackground(final RvBase a, final RxBackgroundInterface rxBackgroundInterface) {
         Observable<List> observable = Observable.just("a") //had to add something
                 .map(new Function<String, List>() {
                     @Override
@@ -32,13 +32,19 @@ public class RxBackground {
         observable.subscribe(a.buildObserver(new RvBase.ObserverInterface() {
             @Override
             public void onNext(List value) {
-                rxBackgroundInterface.getOnResultReceived(value);
+                //rxBackgroundInterface.getOnResultReceived(value);
+                if (value != null) {
+                    a.populateRv(value);
+                } else {
+                    new ErrorNotifications().displayError(a, "Returned list is null");
+                }
+
             }
         }));
     }
 
     public interface RxBackgroundInterface {
         public List getDoInBackground();
-        public void getOnResultReceived(List value);
+        //public void getOnResultReceived(List value);
     }
 }
