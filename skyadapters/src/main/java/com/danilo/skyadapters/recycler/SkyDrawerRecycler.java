74379716a41b904024e6a3524cabad5a6ea31b3d
Danilo <danilot.89@gmail.com>
2018-01-15 14:19:49 +0100

    commit

--- skyadapters/src/main/java/com/danilo/skyadapters/recycler/SkyDrawerRecycler.java
+++ skyadapters/src/main/java/com/danilo/skyadapters/recycler/SkyDrawerRecycler.java
@@ -32,9 +32,6 @@ public class SkyDrawerRecycler extends RecyclerView {
         customRow = ta.getResourceId(R.styleable.SkyDrawerRecycler_drawerCustomRow, 0);
 
         menuID = ta.getResourceId(R.styleable.SkyDrawerRecycler_drawerMenu, R.menu.default_drawer_items);
-        if (customRow.equals(0)) {
-            new ErrorNotifications().displayError(context, "Add app:drawerMenu= into SkyDrawerRecycler xml");
-        }
 
         int span = ta.getInt(R.styleable.SkyDrawerRecycler_drawerNumberOfRows, 1);
 
