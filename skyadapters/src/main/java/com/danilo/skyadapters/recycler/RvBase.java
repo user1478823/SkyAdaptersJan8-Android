759aa15ecefea2d5e49c69f248991418fdb48d71
Danilo <danilot.89@gmail.com>
2018-01-19 14:30:06 +0100

    commit

--- skyadapters/src/main/java/com/danilo/skyadapters/recycler/RvBase.java
+++ skyadapters/src/main/java/com/danilo/skyadapters/recycler/RvBase.java
@@ -39,7 +39,6 @@ public abstract class RvBase extends AppCompatActivity /*implements EndlessRecyc
             if (EndlessRecyclerOnScrollListener2.onScrollInterface.class.isAssignableFrom(this.getClass())) {
                 onScrollInterface = (EndlessRecyclerOnScrollListener2.onScrollInterface) this;
                 rv.addOnScrollListener(onScrollInterface.onScroll());
-                
             }
 
 
