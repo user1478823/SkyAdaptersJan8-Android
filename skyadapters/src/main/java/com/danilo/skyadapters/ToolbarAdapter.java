bbebd2fe96effeac5b2c3beae75f3576d0d922a8
Danilo <danilot.89@gmail.com>
2018-01-26 16:25:36 +0100

    c

--- skyadapters/src/main/java/com/danilo/skyadapters/ToolbarAdapter.java
+++ skyadapters/src/main/java/com/danilo/skyadapters/ToolbarAdapter.java
@@ -153,7 +153,7 @@ public class ToolbarAdapter {
         ActionBarDrawerToggle toggleBtn = new ActionBarDrawerToggle(a, drawerLayout,
                 R.string.drawer_open, R.string.drawer_closed);
         drawerLayout.addDrawerListener(toggleBtn);
-        //toolbar.setTitle("");
+        toolbar.setTitle("");
         a.setSupportActionBar(toolbar);
         //a.getSupportActionBar().setTitle("Test title");
         a.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -161,10 +161,10 @@ public class ToolbarAdapter {
     }
 
     public ToolbarAdapter setToolbarTitle(String title){
-        //toolbar.setTitle(title);
+        toolbar.setTitle(title);
         //LinearLayout ll = a.findViewById(R.id.ll);
         //ll.setVisibility(View.GONE);
-        a.getSupportActionBar().setTitle(title);
+       // a.getSupportActionBar().setTitle(title);
         //a.setTitle(title);
         return this;
     }
