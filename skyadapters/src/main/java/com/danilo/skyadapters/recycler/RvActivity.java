b24393b4d5bb7105091801e29b02653dca27442b
Danilo <danilot.89@gmail.com>
2018-02-09 10:46:28 +0100

    m

--- skyadapters/src/main/java/com/danilo/skyadapters/recycler/RvActivity.java
+++ skyadapters/src/main/java/com/danilo/skyadapters/recycler/RvActivity.java
@@ -207,6 +207,7 @@ public abstract class RvActivity extends AppCompatActivity {
             for (int i = 0; i < list.size(); i++) {
                 if (list.get(i) == value.get(0)) {
                     duplicate = true;
+                    break;
                 }
             }
             if (!duplicate) {
